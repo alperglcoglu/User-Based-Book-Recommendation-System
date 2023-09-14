b246623a3b4d4ada9bcfc46dcd81222c8e28a9d0
Alper <alper.gulcoglu@yildiz.edu.tr>
2023-09-14 17:07:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 from main import recommend_book
 
-st.header("Book Recommendation System")
+st.title("Book Recommender")
 model = pickle.load(open('models/model.pkl','rb'))
 book_names = pickle.load(open('models/book_names.pkl','rb'))
 final_rating = pickle.load(open('models/final_rating.pkl','rb'))
